4908a7dc4820f8aaba2b9de56bf76eb73a4a72fb
gokul <gokul465eswaran@gmail.com>
2023-07-29 22:22:16 +0530

    stripeConfig

---
+++ src/services/stripeConfig.js
@@ -0,0 +1,7 @@
+const stripeConfig = {
+  publicKey:
+    "pk_test_51L6siTSEAdM8gCR0iXJhpWIeNRt9cfGKEMhDZCAQnW7kcOoECXxrYaaep2TFfgszNOcA1z8HI0EQDFphkYuwpnDN00EWaRLXIs",
+  secretKey:
+    "sk_test_51L6siTSEAdM8gCR0yoXtl339iV6ZeSWCmt2jEioez6RKoa5ni16simr0ZqKu86PncRPqJvMV5r8sQTRUlddDZunB009f7ggsxs",
+};
+export default stripeConfig;
